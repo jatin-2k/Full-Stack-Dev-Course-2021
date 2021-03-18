feae92042b61dea58ee66d34d20738af80324583
Jatin Agrawal <jatinagrawal2000@gmail.com>
2021-03-18 17:54:00 +0530

    Redux Thunk

--- Course2/confusion/src/components/DishDetailComponent.js
+++ Course2/confusion/src/components/DishDetailComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CommentForm from './CommentFormComponent';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 
     function stringToDate(dateString){
         var date = new Date(Date.parse(dateString));
@@ -42,7 +42,25 @@ import { Link } from 'react-router-dom';
 
     function DishDetail(props){
 
-        if(props.dish!=null){
+        if(props.isLoading){
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading/>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errmess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <h4>{props.errmess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if(props.dish != null){
 
             return(
                 <div className="container">
